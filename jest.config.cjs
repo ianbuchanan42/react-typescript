e8285ebeb64dba9ddae8c503a060f3eb2ab94eff
Ian Buchanan <get.ian.buchanan@gmail.com>
2025-03-18 14:00:35 -0700

    chore: add Jest configuration files
    - Add jest.config.cjs for Jest configuration
    
    - Add babel.config.cjs for Babel configuration
    
    - Add tsconfig.jest.json for TypeScript configuration in tests

---
+++ jest.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+        useESM: true,
+        diagnostics: {
+          ignoreCodes: [151001],
+        },
+      },
+    ],
+    '^.+\\.js$': 'babel-jest',
+  },
+  testMatch: ['**/__tests__/**/*.test.tsx'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
